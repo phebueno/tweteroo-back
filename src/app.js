bb9584449c096c6d82d4885cd060d72f489e12ea
Phebueno <fcbgomes@gmail.com>
2023-04-06 15:56:55 -0300

    Adicionadas validações para posts

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import cors from "cors";
 //import tweets from "./data/tweetsData.js";
 
 //Variáveis globais para armazenamento
-const tweets=[];
+const tweets = [];
 const users = [
   {
     username: "bobesponja",
@@ -12,7 +12,7 @@ const users = [
   },
   {
     username: "bobba",
-    avatar: "a"
+    avatar: "a",
   },
 ];
 
@@ -23,20 +23,41 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+function validate400(...fields) {
+  let error = "";
+  let message = "OK";
+  fields.forEach((field) => {
+    if (!field || typeof field !== "string") {
+      console.log("achou erro");
+      error = 400;
+      message = "Todos os campos são obrigatórios!";
+    }
+  });
+  return { error, message };
+}
+
 //Registro do usuário
 app.post("/sign-up", (req, res) => {
   const { username, avatar } = req.body;
-  users.push({ username, avatar });
-  res.send("OK");
+  const { error, message } = validate400(username, avatar);
+  if (error) return res.status(error).send(message);
+  else {
+    users.push({ username, avatar });
+    res.status(201).send(message);
+  }
 });
 
 //Post do tweet
 app.post("/tweets", (req, res) => {
   const { username, tweet } = req.body;
-  const usuarioCadastrado = users.find((user) => user.username === username);
-  if (!usuarioCadastrado) return res.send("UNAUTHORIZED");
-  tweets.push({ username, tweet });
-  res.send("OK");
+  const { error, message } = validate400(username, tweet);
+  if (error) return res.status(error).send(message);
+  else {
+    const usuarioCadastrado = users.find((user) => user.username === username);
+    if (!usuarioCadastrado) return res.status(401).send("UNAUTHORIZED");
+    tweets.push({ username, tweet });
+    res.status(201).send(message);
+  }
 });
 
 //Retorna 10 últimos tweets
@@ -47,6 +68,7 @@ app.get("/tweets", (req, res) => {
     avatar: users.find((user) => user.username === tweet.username).avatar, //vai dar erro se o usuário procurado não estiver cadastrado
   }));
   res.send(newestTenTweetsAvatar);
+  //Adicionar .reverse() para que inverter a ordem em que os tweets são enviados
 });
 
 const PORT = 5000;
