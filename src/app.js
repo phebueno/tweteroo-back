69a0c7fa0ee96e83837c2f26a02907b018afe60b
Phebueno <fcbgomes@gmail.com>
2023-04-06 02:39:43 -0300

    Adicionadas funçoes post e get de 10 últimos tweets

--- src/app.js
+++ src/app.js
@@ -1,10 +1,20 @@
 import express from "express";
-import cors from 'cors';
-//import tweetsData from "./data/tweetsData.js";
+import cors from "cors";
+import tweets from "./data/tweetsData.js";
 
 //Variáveis globais para armazenamento
-const tweets=[];
-const users=[];
+//const tweets=[];
+const users = [
+  {
+    username: "bobesponja",
+    avatar:
+      "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png",
+  },
+  {
+    username: "bobba",
+    avatar: "a"
+  },
+];
 
 //Criação do App Servidor
 const app = express();
@@ -14,23 +24,30 @@ app.use(cors());
 app.use(express.json());
 
 //Registro do usuário
-app.post("/sign-up",(req,res)=>{
-    const {username, avatar} = req.body;
-    users.push({username, avatar})
-    res.send("OK");
+app.post("/sign-up", (req, res) => {
+  const { username, avatar } = req.body;
+  users.push({ username, avatar });
+  res.send("OK");
 });
 
 //Post do tweet
-app.post("/tweets",(req,res)=>{
-    res.send("Postado com sucesso!");
+app.post("/tweets", (req, res) => {
+  const { username, tweet } = req.body;
+  const usuarioCadastrado = users.find((user) => user.username === username);
+  if (!usuarioCadastrado) return res.send("UNAUTHORIZED");
+  tweets.push({ username, tweet });
+  res.send("OK");
 });
 
 //Retorna 10 últimos tweets
-app.get("/tweets",(req,res)=>{
-    const newestTenTweets = tweets;
-    res.send(newestTenTweets);
+app.get("/tweets", (req, res) => {
+  const newestTenTweets = tweets.slice(-10);
+  let newestTenTweetsAvatar = newestTenTweets.map((tweet) => ({
+    ...tweet,
+    avatar: users.find((user) => user.username === tweet.username).avatar, //vai dar erro se o usuário procurado não estiver cadastrado
+  }));
+  res.send(newestTenTweetsAvatar);
 });
 
 const PORT = 5000;
-app.listen(PORT, ()=>console.log(`Servidor rodando na porta ${PORT}`));
-
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
--- src/data/tweetsData.js
+++ src/data/tweetsData.js
@@ -1,22 +1,60 @@
-const tweetsData = [
+const tweets = [
   {
     username: "bobesponja",
-    avatar:
-      "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png",
     tweet: "Eu amo hambúrguer de siri!",
   },
   {
-    username: "bobesponjeee",
-    avatar:
-      "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png",
+    username: "bobesponja",
+    tweet: "Eu amo hambúrguer",
+  },
+  {
+    username: "bobesponja",
+    tweet: "Eu amo hambúrguer!",
+  },
+  {
+    username: "bobesponja",
     tweet: "Eu amo hambúrguer de siri!",
   },
   {
-    username: "bobesponjoo",
-    avatar:
-      "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png",
+    username: "bobesponja",
     tweet: "Eu amo hambúrguer de siri!",
-  }
+  },
+  {
+    username: "bobba",
+    tweet: "Eu amo hambúrguer de siri!",
+  },
+  {
+    username: "bobba",
+    tweet: "Eu amo hambúrguer de siri!",
+  },
+  {
+    username: "bobba",
+    tweet: "Eu amo hambúrguer de siri!",
+  },
+  {
+    username: "bobba",
+    tweet: "Eu amo hambúrguer de siri!",
+  },
+  {
+    username: "bobba",
+    tweet: "Eu amo hambúrguer de siri!",
+  },
+  {
+    username: "bobba",
+    tweet: "Eu amo hambúrguer de siri!",
+  },
+  {
+    username: "bobba",
+    tweet: "Eu amo hambúrguer de siri!",
+  },
+  {
+    username: "bobba",
+    tweet: "Eu amo hambúrguer de siri!",
+  },
+  {
+    username: "bobesponja",
+    tweet: "Eu amo hambúrguer de caranguejo!",
+  },
 ];
 
-export default tweetsData;
+export default tweets;
